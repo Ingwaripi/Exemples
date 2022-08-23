454959260a932f36f544be56ffb5e5956c5d43c2
Ekaterina <ekaterina266226@mail.ru>
2022-08-23 14:12:51 +0300

    add Example 16

---
+++ Example016/Program.cs
@@ -0,0 +1,9 @@
+﻿int Fibonaci(int n)
+{
+    if (n == 1 || n == 2) return 1;
+    else return Fibonaci(n - 1) + Fibonaci(n - 2);
+}
+for (int i = 1; i < 10; i++)
+{
+    Console.WriteLine(Fibonaci(i));
+}
